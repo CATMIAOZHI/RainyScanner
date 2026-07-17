c03af37f0601ca63538a6f2c4a8fce2a36d97217
CATMIAOZHI <CATMIAOZHI@users.noreply.github.com>
2026-07-17 20:17:54 +0000

    release: v1.2.0 - CI/CD 修复 + 安全签名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.rainyscanner.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
